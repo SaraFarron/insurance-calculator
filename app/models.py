4f8c64c5d97f605f5e33443e7c33b32e693fd052
NikitaSiulin <n.siulin@rufago.ru>
2024-11-18 17:19:49 +0700

    feat: added models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,28 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import DECIMAL, Column, Date, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
 
-class User(Base):
-    __tablename__ = "users"
+class Upload(Base):
+    __tablename__ = "uploads"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    cargo_date = Column(Date)
+    cargos = relationship("Cargo", back_populates="upload")
+
+
+class Cargo(Base):
+    __tablename__ = "cargos"
+    id = Column(Integer, primary_key=True, index=True)
+    cargo_type = Column(String)
+    cargo_rate = Column(DECIMAL(10, 2))
+    upload_id = Column(Integer, ForeignKey("uploads.id"))
+    upload = relationship("Upload", back_populates="cargos")
+
+
+class Tariff(Base):
+    __tablename__ = "tariffs"
+    id = Column(Integer, primary_key=True, index=True)
+    cargo_type = Column(String)
+    created_at = Column(Date)
+    price = Column(DECIMAL(10, 2))
